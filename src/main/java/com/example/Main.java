c5f518397d601713b0e71dd4c16ff49709ec5397
Alan Evans <thealanevans@gmail.com>
2017-12-13 13:35:07 -0400

    Generate a mnemonic

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,19 @@
 package com.example;
 
+import io.github.novacrypto.bip39.MnemonicGenerator;
+import io.github.novacrypto.bip39.Words;
+import io.github.novacrypto.bip39.wordlists.English;
+
+import java.security.SecureRandom;
+
 public final class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nothing here yet");
+        StringBuilder sb = new StringBuilder();
+        byte[] entropy = new byte[Words.TWELVE.byteLength()];
+        new SecureRandom().nextBytes(entropy);
+        new MnemonicGenerator(English.INSTANCE)
+                .createMnemonic(entropy, sb::append);
+        System.out.println(sb.toString());
     }
 }
